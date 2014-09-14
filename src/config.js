d6011183f601ea2edc8e49a694c65e72d9f9c939
petejscott <pete.j.scott@gmail.com>
2014-09-14 16:19:13 -0400

    Remove unused maxWords option from config.js (resolves issue #11)

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@ var wordScramble = wordScramble || {};
 wordScramble.configuration = {
 	letterCount : 5,
 	minWords : 5,
-	maxWords: 8,
 	minWordLength : 3,
 	dictionaryUrl : 'http://localhost/projects/wordScramble/src/dict/custom-en-us.txt'
 };
