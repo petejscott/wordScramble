03bdead935af6f91ce2c8ebb6e5625416758aa1d
Pete Scott <pete.j.scott@gmail.com>
2016-04-01 20:15:50 -0400

    Disable submit until min word length is met

--- src/js/uiModules/submitWordManager.js
+++ src/js/uiModules/submitWordManager.js
@@ -21,13 +21,41 @@ wordScramble.submitWordManager = (function(pubsub)
 		el.removeEventListener(
 			'click', manager.eventHandler, false);
 	}
+	
 	function setup()
 	{
 		var el = getElement();
 		if (el === null) return;
 
+		el.disabled = true;
+		pubsub.subscribe("wordScramble/wordAttemptUpdated", function(topic, data)
+		{
+			var canSubmit = isSubmitCandidate(data.wordString);
+			if (canSubmit && el.disabled) {
+				enable(el, data.wordString);
+			}
+			else if (!canSubmit && !el.disabled)
+			{
+				disable(el);
+			}
+		});
+		
 		el.addEventListener(
 			'click', manager.eventHandler, false);
+	}
+	
+	function isSubmitCandidate(wordString) {
+		return wordString.length >= wordScramble.configuration.minWordLength;
+	}
+	
+	function disable(el) {
+		el.disabled = true;
+		el.value = "...";
+	}
+	
+	function enable(el, word) {
+		el.disabled = false;
+		el.value = "Submit Word";
 	}
 
 	manager.eventHandler = function(evt)
